3c851bca3ff6b12225011b71ef386c7ac8d4eb6c
ammarahmad <ammar.ahmad6410@gmail.com>
2023-10-07 14:07:11 +0500

    header done.

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Header from "@/components/Header";
+import Head from "next/head";
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>Amazon</title>
+			</Head>
+
+			{/* Header */}
+			<Header />
+		</div>
+	);
+}
